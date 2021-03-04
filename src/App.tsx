2af67992acfc82b882312b042efe619b897df1bc
black-sheep777 <forough.fallahfar@gmail.com>
2021-03-04 16:47:02 +0330

    chore(login): add react-router-dom & downgrade two packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react'
-import logo from 'logo.svg'
+// import Login from 'components/login/login'
 import 'App.scss'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {/* <Switch>
+      <Route exact path="/" component={Login} />
+    </Switch> */}
+    shitttttt
+    </div> 
+   
+
   )
 }
 
