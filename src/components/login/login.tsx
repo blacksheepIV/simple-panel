754924eb7c9ab656d8a5682deb1f654f9974ee7f
black-sheep777 <forough.fallahfar@gmail.com>
2021-03-05 12:08:12 +0330

    feat(login): add react-hook-form for login validation

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import 'components/login/login.scss'
 
+// type Inputs = {
+//   username: string,
+//   password: string
+// }
+  
 const Login = function (){
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  function authenticate(e: React.FormEvent){
-    e.preventDefault()
-    console.log(e)
+
+  const { register, handleSubmit, errors } = useForm()
+  function authenticate(data : React.FormEvent){
+    
+    console.log(data)
   }
   return (
 
     <div className="login">
-      <form className="login__form" onSubmit={authenticate}>
+      <form className="login__form" onSubmit={handleSubmit(authenticate)}>
         <div className="login__header">Login</div>
-        <input className="login__input" placeholder="Username" type="text" value={username} onChange={(e)=>setUsername(e.target.value)} />
-        <input className="login__input" placeholder="Password" type="password" />
+        <input className="login__input" placeholder="Username" type="text" name="username" ref={register({ required:true })} />
+        {errors.username && <span className="login__error">username is required</span>}
+        <input className="login__input" placeholder="Password" type="password" name="password" ref={register({ required:true, minLength:8 })} />
+        {errors.password && errors.password.type === 'required' && <span className="login__error">password is required</span>}
+        {errors.password && errors.password.type === 'minLength' && <span className="login__error">password must be minimum of 8 characters</span>}
         <button className="login__submit" type="submit">Submit</button>
       </form>
     </div>
