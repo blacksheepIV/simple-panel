41355c1cb9e39af82846b223c97abd9f36c5734b
black-sheep777 <forough.fallahfar@gmail.com>
2021-03-07 08:47:46 +0330

    feat(panel): Add user's detail page(UDP)

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Login from 'components/login/login'
 import Panel from 'components/panel/panel'
+import UserDetailPage from 'components/udp/udp'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
@@ -11,6 +12,7 @@ function App() {
         <Switch>  
           <Route exact path="/" component={Login} />
           <Route path="/panel" component={Panel} />
+          <Route path="/user/:userId" component={UserDetailPage} />
         </Switch>
       </div>
     </Router>
